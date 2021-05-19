ea5f01c43de352c2e81ccf8e44c697e052f13820
Theo Smith <80037733+TheodorusSmith@users.noreply.github.com>
2021-05-19 15:14:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello Winc Academy");
+let name = 'Theo';
+console.log(name);
+let a = 2;
+let b = a + 6;
+console.log(b);
+let c = b - 4;
+console.log(c);
+let d = c * b;
+console.log(d);
+let e = d / a;
+console.log(e);
+let f = e % 5;
+console.log(f)
+let age = '53';
+console.log('53');
+
+
+
+
